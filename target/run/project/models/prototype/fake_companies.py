53ce741495875d2b8b01d283e86610bc5f7a4fcb
Ben W <ben@raydata.co>
2023-03-06 21:42:47 +0000

    Try with duckdb committed

--- target/run/project/models/prototype/fake_companies.py
+++
@@ -1,8 +0,0 @@
-
-
-    insert into "dbt"."main"."fake_companies" ("id", "name")
-    (
-        select "id", "name"
-        from "fake_companies__dbt_tmp20230224232716132766"
-    )
-  
--- target/run/project/models/prototype/fake_users.py
+++
@@ -1,8 +0,0 @@
-
-
-    insert into "dbt"."main"."fake_users" ("id", "name")
-    (
-        select "id", "name"
-        from "fake_users__dbt_tmp20230224232716679695"
-    )
-  
